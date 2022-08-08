d056bf5567ded628c28f9359293d2e556c495044
alinasnapyr <alina@snapyr.com>
2022-08-08 12:30:55 +0200

    [MEKA-376] React Native SDK updates

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,38 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
+import { setPushNotificationToken } from 'snapyr-react-native-sdk';
+
+PushNotification.configure({
+    onRegister: function (token:{token: string }) {
+      (async () => {
+        try {
+          console.log("indexToken", token)
+          await setPushNotificationToken(token.token);
+        } catch (ex) {
+          console.log(ex);
+        }
+      })();
+      console.log("TOKEN:", token.token);
+    },
+    onNotification: function (notification) {
+      console.log("NOTIFICATION:", notification);
+      notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+
+    permissions: {
+      alert: true,
+      badge: true,
+      sound: true,
+    },
+
+    popInitialNotification: true,
+  
+    requestPermissions: true,
+  }); 
+
+
 
 AppRegistry.registerComponent(appName, () => App);
