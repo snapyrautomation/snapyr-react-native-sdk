911b3d7bfbc6a70eddfb26f85f40991e2f329ee8
Paul Smith <paul@snapyr.com>
2022-10-13 23:57:17 -0400

    Revert "Remove in-app event listener til v1.1.0"
    This reverts commit fb26fb6988645dd687cb12aa67f71543af762ab1.

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ const SNAPYR_LISTENER_REGISTER = 'snapyrDidRegister';
 const SNAPYR_LISTENER_NOTIFICATION = 'snapyrDidReceiveNotification';
 const SNAPYR_LISTENER_NOTIFICATION_RESPONSE =
   'snapyrDidReceiveNotificationResponse';
+const SNAPYR_LISTENER_INAPP_MESSAGE = 'snapyrInAppMessage';
 
 const LINKING_ERROR =
   `The package 'snapyr-react-native-sdk' doesn't seem to be linked. Make sure: \n\n` +
@@ -76,6 +77,20 @@ export function onSnapyrDidReceiveNotification(
   // Remove/unsubscribe previous listener, if any
   _eventListeners.get(SNAPYR_LISTENER_NOTIFICATION)?.remove();
   _eventListeners.set(SNAPYR_LISTENER_NOTIFICATION, listener);
+}
+
+export function onSnapyrInAppMessage(
+  callback: (message: SnapyrInAppMessage) => void
+): void {
+  const listener = SnapyrEmitter.addListener(
+    SNAPYR_LISTENER_INAPP_MESSAGE,
+    (message: SnapyrInAppMessage) => {
+      callback(message);
+    }
+  );
+  // Remove/unsubscribe previous listener, if any
+  _eventListeners.get(SNAPYR_LISTENER_INAPP_MESSAGE)?.remove();
+  _eventListeners.set(SNAPYR_LISTENER_INAPP_MESSAGE, listener);
 }
 
 export function onSnapyrDidReceiveNotificationResponse(
